03d713273597182c44206849fa3542bcc93d1e4b
LorriAnn <LorriAnn7@att.net>
2021-03-17 11:54:41 -0400

    analysis adds minimum and maximum number

--- codes/arrayAnalysis.js
+++ codes/arrayAnalysis.js
@@ -6,8 +6,28 @@ const average = (array) => {
   return sum/(array.length);
 }
 
+const minimum = (array) => {
+  let minNumber = array[0];
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] < minNumber) {
+      minNumber = array[i];
+    };
+  }
+  return minNumber;
+}
+
+const maximum = (array) => {
+    let maxNumber = array[0];
+    for(let i = 0; i < array.length; i++) {
+      if (array[i] > maxNumber) {
+        maxNumber = array[i];
+      };
+    }
+    return maxNumber;
+  }
+
 const length = (array) => {
   return array.length;
 }
   
-  module.exports = {average, length};
+  module.exports = {average, minimum, maximum, length};
--- tests/arrayAnalysis.test.js
+++ tests/arrayAnalysis.test.js
@@ -1,4 +1,4 @@
-const {average, length} = require('../codes/arrayAnalysis');
+const {average, minimum, maximum, length} = require('../codes/arrayAnalysis');
 
 const data = [1, 8, 3, 4, 2, 6];
 
@@ -6,13 +6,13 @@ test('returns average of array', () => {
   expect(average(data)).toEqual(4);
 });
 
-// test('returns minimum number', () => {
-//   expect(minimum(1, 8, 3, 4, 2, 6)).toEqual(1);
-// })
+test('returns minimum number', () => {
+  expect(minimum(data)).toEqual(1);
+})
 
-// test('returns maximum number', () => {
-//   expect(maximum(1, 8, 3, 4, 2, 6)).toEqual(8);
-// })
+test('returns maximum number', () => {
+  expect(maximum(data)).toEqual(8);
+})
 
 test('returns number of items in an array', () => {
     expect(length(data)).toEqual(6);
